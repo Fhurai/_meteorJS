46b3459df3917df838eab43b43189d14e12cde7e
Fhurai <kulu57@gmail.com>
2025-05-05 14:29:41 +0200

    fix: ensure unique checkbox name and id for each task

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -3,6 +3,8 @@ import React from "react";
 export const Task = ({ task, onCheckboxClick }) => {
     return <li>
         <input
+            name={"task_" + task._id}
+            id={"task_" + task._id}
             type="checkbox"
             checked={!!task.isChecked}
             onClick={() => onCheckboxClick(task)}
